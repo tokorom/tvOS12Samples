7a306d17c4ae0b8a398de5d89a5e087efd5a676c
tokorom <tokorom@gmail.com>
2018-08-26 17:20:29 +0900

    Add some assets

--- app/code/dataSource/SampleButtonObjects.swift
+++ app/code/dataSource/SampleButtonObjects.swift
@@ -10,10 +10,10 @@ class SampleButtonObjects {
     static let shared = SampleButtonObjects()
 
     let samples: [ButtonObject] = [
-        ButtonObject(content: ButtonObject.Content.text("Preview"), title: "Preview", subtitle: nil),
+        ButtonObject(content: ButtonObject.Content.image("QuickLook"), title: "Quick Look", subtitle: nil),
         ButtonObject(content: ButtonObject.Content.text("$19.99"), title: "Buy", subtitle: "With iTunes Extras"),
         ButtonObject(content: ButtonObject.Content.text("$5.99"), title: "Rent", subtitle: nil),
-        ButtonObject(content: ButtonObject.Content.text("Wish List"), title: "Wish List", subtitle: nil),
+        ButtonObject(content: ButtonObject.Content.image("CloudSharing"), title: "Share", subtitle: nil),
         ButtonObject(content: ButtonObject.Content.text("No Title"), title: nil, subtitle: nil),
     ]
 }
