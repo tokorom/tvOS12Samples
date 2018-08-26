b1c3b90358fda86b3b435ace2debd1df09d478a6
tokorom <tokorom@gmail.com>
2018-08-27 01:44:17 +0900

    Add Author samples

--- app/code/dataSource/SampleAuthors.swift
+++ app/code/dataSource/SampleAuthors.swift
@@ -10,6 +10,10 @@ class SampleAuthors {
     static let shared = SampleAuthors()
 
     let samples: [Author] = [
-        Author(name: .simple("Yuta Tokoro", "Spinners"), imageName: "tokorom"),
+        Author(name: .simple("所 友太", "Spinners"), imageName: "tokoro"),
+        Author(name: .simple("堤 修一", "Fyusion"), imageName: "tsusumi"),
+        Author(name: .simple("吉田 悠一", "デンソーアイティーラボラトリ"), imageName: "yoshida"),
+        Author(name: .simple("加藤 尋樹", "はてな"), imageName: "kato"),
+        Author(name: .simple("池田 翔", "はてな"), imageName: "ikeda"),
     ]
 }
