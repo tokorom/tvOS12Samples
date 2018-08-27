e7aca1d4dda15796d394756f42d527006a1ba957
tokorom <tokorom@gmail.com>
2018-08-27 12:33:49 +0900

    Add some sample views

---
+++ app/code/controller/TVDigitEntrySampleViewController.swift
@@ -0,0 +1,10 @@
+//
+//  TVDigitEntrySampleViewController.swift
+//
+//  Created by ToKoRo on 2018-08-27.
+//
+
+import UIKit
+
+class TVDigitEntrySampleViewController: UITableViewController {
+}
---
+++ app/code/controller/TVLockupViewSampleController.swift
@@ -0,0 +1,10 @@
+//
+//  TVLockupViewSampleController.swift
+//
+//  Created by ToKoRo on 2018-08-27.
+//
+
+import UIKit
+
+class TVLockupViewSampleController: UIViewController {
+}
