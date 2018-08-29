54286716815d9dcfcec1c0024952edd31be0f651
tokorom <tokorom@gmail.com>
2018-08-29 19:29:53 +0900

    Add iOS sample for password autofill

---
+++ iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by tokorom on 2018/08/29.
+//  Copyright © 2018年 Spinners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
