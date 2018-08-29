2f877ae1543b7a745925a28ef05ccb8c61fbc1b4
tokorom <tokorom@gmail.com>
2018-08-30 06:24:41 +0900

    Add Password Auto Fill Sample

---
+++ app/code/controller/PasswordAutoFillSampleViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PasswordAutoFillSampleViewController.swift
+//
+//  Created by ToKoRo on 2018-08-30.
+//
+
+import UIKit
+
+class PasswordAutoFillSampleViewController: UIViewController {
+    @IBOutlet weak var idTextField: UITextField?
+    @IBOutlet weak var logInButton: UIButton?
+}
