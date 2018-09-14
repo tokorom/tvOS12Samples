a9133e260d61d24aa274e8c40ae95e1da3c2db44
tokorom <tokorom@gmail.com>
2018-09-15 02:26:00 +0900

    Improve ScrollableCanvasView

--- app/code/view/ScrollableCanvasView.swift
+++ app/code/view/ScrollableCanvasView.swift
@@ -8,52 +8,42 @@ import UIKit
 
 class ScrollableCanvasView: CanvasView, UIFocusItemScrollableContainer {
     override func createRects() -> [Rect] {
-        var rects: [Rect] = []
-
-        let rectWidth: CGFloat = 100
-        let margin: CGFloat = 100
-        var x: CGFloat = 100
-
-        while x + rectWidth < contentSize.width {
-            let rect = MoveRect(x: x, y: 100, color: UIColor.blue.cgColor, parent: self)
-            rects.append(rect)
-            x += rectWidth + margin
-        }
-
-        return rects
+        return (0..<30)
+            .map { index in
+                let color = UIColor(
+                    red: 1 - CGFloat(index + 1) / 30,
+                    green: 0.5,
+                    blue: CGFloat(index + 1) / 30,
+                    alpha: 1
+                )
+                return MoveRect(
+                    x: 100 + 200 * CGFloat(index),
+                    y: 100,
+                    color: color.cgColor,
+                    parent: self
+                )
+            }
     }
 
     override func draw(_ rect: CGRect) {
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return
+        if contentOffset != .zero, let context = UIGraphicsGetCurrentContext() {
+            context.translateBy(x: -contentOffset.x, y: -contentOffset.y)
         }
-
-        if renderOffset != .zero {
-            context.translateBy(x: -renderOffset.x, y: -renderOffset.y)
-        }
-
-        for rect in rects {
-            rect.draw(in: context)
-        }
-    }
-
-    var renderOffset: CGPoint = .zero
-
-    var contentOffset: CGPoint {
-        get {
-            return renderOffset
-        }
-        set {
-            self.renderOffset = newValue
-            setNeedsDisplay()
-        }
-    }
-
-    var contentSize: CGSize {
-        return CGSize(width: bounds.size.width * 2, height: bounds.size.height)
+        super.draw(rect)
     }
 
     var visibleSize: CGSize {
         return bounds.size
+    }
+
+    var contentSize: CGSize {
+        let width = (rects.last?.frame.maxX ?? 0) + 100
+        return CGSize(width: width, height: visibleSize.height)
+    }
+
+    var contentOffset: CGPoint = .zero {
+        didSet {
+            setNeedsDisplay()
+        }
     }
 }
